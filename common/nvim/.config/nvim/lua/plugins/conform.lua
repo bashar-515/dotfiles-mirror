5ba52eb7b1caf0ba8fe8f0689923dffbc871f92d
Bashar Eid <beesho.eid@gmail.com>
2026-05-12 23:48:04 -0400

    clean up formatting and linting

--- common/nvim/.config/nvim/lua/plugins/conform.lua
+++ common/nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,50 +1,76 @@
-vim.pack.add { gh "stevearc/conform.nvim" }
+vim.pack.add({ gh("stevearc/conform.nvim") })
 
 local fts_by_formatter = {
-  oxfmt = {
-    "javascript",
-    "typescript",
-    "javascriptreact",  -- React, Solid, Qwik, Preact, Lit, Alptine.js (Next.js, Nuxt, Remix, TanStack Start, React Router, React Native)
-    "typescriptreact",
-    "vue",
-    "css",
-    "scss",
-    "html",
-    "json",
-    "jsonc",
-    "yaml",
-    "markdown",
-    "graphql",
-    "toml",
-    "less",
-  },
+	oxfmt = {
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"vue",
+		"css",
+		"scss",
+		"html",
+		"json",
+		"jsonc",
+		"yaml",
+		"markdown",
+		"graphql",
+		"toml",
+		"less",
+	},
+
+	prettier = {
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"htmlangular",
+		"vue",
+		"css",
+		"less",
+		"scss",
+		"html",
+		"handlebars",
+		"json",
+		"jsonc",
+		"graphql",
+		"markdown",
+		"mdx",
+		"yaml",
+	},
+
+	stylua = {
+		"lua",
+	},
 }
 
-local conform = require "conform"
-local util = require "conform.util"
+local conform = require("conform")
+local util = require("conform.util")
 
-local formatters_by_ft = require"util".invert_index(fts_by_formatter)
+local formatters_by_ft = require("util").invert_index(fts_by_formatter)
 
-formatters_by_ft.dockerfile = { lsp_format = "last" }
+conform.setup({
+	formatters_by_ft = formatters_by_ft,
 
-conform.setup {
-  formatters_by_ft = formatters_by_ft,
+	formatters = {
+		oxfmt = {
+			command = util.find_executable({ "node_modules/.bin/oxfmt" }, "oxfmt"),
+		},
 
-  formatters = {
-    oxfmt = {
-      command = util.find_executable({ "node_modules/oxfmt/bin/oxfmt" }, "oxfmt"),
-    },
-  },
+		prettier = {
+			command = util.find_executable({ "node_modules/.bin/prettier" }, "prettier"),
+		},
+	},
 
-  default_format_opts = {
+	default_format_opts = {
 		timeout_ms = 1000,
 		lsp_format = "never",
 	},
 
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = "never"
-  },
-}
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "never",
+	},
+})
 
 vim.keymap.set({ "n", "v" }, "<leader>f", conform.format)
--- common/nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ common/nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,43 +1,61 @@
-vim.pack.add { gh "mfussenegger/nvim-lint" }
+vim.pack.add({ gh("mfussenegger/nvim-lint") })
 
 local fts_by_linter = {
-  oxlint = {
-    "javascript",
-    "typescript",
-    "javascriptreact",  -- React, Solid, Qwik, Preact, Lit, Alptine.js (Next.js, Nuxt, Remix, TanStack Start, React Router, React Native)
-    "typescriptreact",
-    "vue",
-  },
+	oxlint = {
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"vue",
+	},
 
-  eslint = {
-    "javascript",
-    "typescript",
-    "javascriptreact",
-    "typescriptreact",
-    "vue",
-  }
+	eslint = {
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"vue",
+	},
 }
 
-local lint = require"lint"
+local lint = require("lint")
 
-lint.linters_by_ft = require"util".invert_index(fts_by_linter)
+lint.linters_by_ft = require("util").invert_index(fts_by_linter)
 
 local function find_local_bin(relative_path)
-  local local_bin = vim.fn.findfile(relative_path, vim.fn.getcwd() .. ";")
-  return local_bin ~= "" and local_bin or nil
+	local local_bin = vim.fn.findfile(relative_path, vim.fn.getcwd() .. ";")
+	return local_bin ~= "" and local_bin or nil
 end
 
-lint.linters.oxlint.cmd = function() return find_local_bin "node_modules/.bin/oxlint" end
-lint.linters.eslint.cmd = function() return find_local_bin "node_modules/.bin/eslint" end
+lint.linters.oxlint.cmd = function()
+	return find_local_bin("node_modules/.bin/oxlint")
+end
+lint.linters.eslint.cmd = function()
+	return find_local_bin("node_modules/.bin/eslint")
+end
+
+local function available_for(ft)
+	local available = {}
+	for _, name in ipairs(lint.linters_by_ft[ft] or {}) do
+		local linter = lint.linters[name]
+		local cmd = type(linter.cmd) == "function" and linter.cmd() or linter.cmd
+		if cmd then
+			table.insert(available, name)
+		end
+	end
+	return available
+end
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-  group = lint_augroup,
+	group = lint_augroup,
 
-  callback = function()
-    lint.try_lint()
-  end,
+	callback = function()
+		lint.try_lint(available_for(vim.bo.filetype))
+	end,
 
-  vim.keymap.set("n", "<leader>l", function() lint.try_lint() end)
+	vim.keymap.set("n", "<leader>l", function()
+		lint.try_lint(vim.bo.filetype)
+	end),
 })
