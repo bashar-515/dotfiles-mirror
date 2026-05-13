c533b4c4720c9b1b3bec4769ee7e83a37188abdf
Bashar Eid <bashar@withclad.com>
2026-05-13 10:45:32 -0400

    conditionally load formatters

--- common/nvim/.config/nvim/lua/plugins/conform.lua
+++ common/nvim/.config/nvim/lua/plugins/conform.lua
@@ -49,16 +49,40 @@ local util = require("conform.util")
 
 local formatters_by_ft = require("util").invert_index(fts_by_formatter)
 
+local function is_direct_dep(pkg)
+	return function(_, ctx)
+		local start = ctx.filename and vim.fs.dirname(ctx.filename) or vim.uv.cwd()
+		local pkg_json = vim.fs.find("package.json", { upward = true, path = start })[1]
+		if not pkg_json then
+			return false
+		end
+		local ok, data = pcall(function()
+			return vim.json.decode(table.concat(vim.fn.readfile(pkg_json), "\n"))
+		end)
+		if not ok or type(data) ~= "table" then
+			return false
+		end
+		for _, field in ipairs({ "dependencies", "devDependencies", "peerDependencies", "optionalDependencies" }) do
+			if data[field] and data[field][pkg] then
+				return true
+			end
+		end
+		return false
+	end
+end
+
 conform.setup({
 	formatters_by_ft = formatters_by_ft,
 
 	formatters = {
 		oxfmt = {
 			command = util.find_executable({ "node_modules/.bin/oxfmt" }, "oxfmt"),
+			condition = is_direct_dep("oxfmt"),
 		},
 
 		prettier = {
 			command = util.find_executable({ "node_modules/.bin/prettier" }, "prettier"),
+			condition = is_direct_dep("prettier"),
 		},
 	},
 
